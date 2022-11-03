da00d28504dd229dcc343aeb03392d763dc9fa82
KeanoMagnifico <roybachinskiy@gmail.com>
2022-11-03 23:10:56 +0300

    v.0.4

--- src/Pawn.java
+++ src/Pawn.java
@@ -10,17 +10,37 @@ public class Pawn extends ChessPiece{
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (((line == toLine) && (column == toColumn)) || (toLine < 0 || toLine > 7) || (toColumn < 0 || toColumn > 7)){
-            return false;
-        } else {
-            if ((color.equals("White")) && (((line == 1) && ((toLine - line) == 2)) || ((line > 1) && ((toLine - line) == 1))) || ((color.equals("Black")) && (((line == 7) && (line - toLine == 2)) || ((line > 1) && (line - toLine == 1)))) && (column == toColumn)){
-                return true;
+        if (checkPosition(line) && checkPosition(column) && checkPosition(toLine) && checkPosition(toColumn)){
+            if (column == toColumn){
+                int start, direction;
+
+                if(color.equals("White")){
+                    start = 1;
+                    direction = 1;
+                } else {
+                    start = 6;
+                    direction = -1;
+                }
+
+                if (line + direction == toLine){
+                    return chessBoard.board[toLine][toColumn] == null;
+                }
+
+                if (line == start && line + 2 * direction == toLine){
+                    return chessBoard.board[toLine][toColumn] == null && chessBoard.board[line + direction][column] == null;
+                }
             } else return false;
         }
+        return false;
     }
 
     @Override
     public String getSymbol() {
         return "P";
+    }
+    public boolean checkPosition(int position){
+        if (position < 0 || position > 7){
+            return false;
+        } else return true;
     }
 }
