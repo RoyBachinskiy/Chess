6f01d72957506cc86962e45bee197e4a6afb5cf9
KeanoMagnifico <roybachinskiy@gmail.com>
2022-11-06 22:57:42 +0300

    v0.5

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,4 +1,4 @@
-public class Bishop extends ChessPiece{
+public class Bishop extends ChessPiece {
     public Bishop(String color) {
         super(color);
     }
@@ -10,17 +10,54 @@ public class Bishop extends ChessPiece{
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (((line == toLine) && (column == toColumn)) || (toLine < 0 || toLine > 7) || (toColumn < 0 || toColumn > 7)){
-            return false;
-        } else {
-            if ((color.equals("White")) && (((line == 1) && ((toLine - line) == 2)) || ((line > 1) && ((toLine - line) == 1))) || ((color.equals("Black")) && (((line == 7) && (line - toLine == 2)) || ((line > 1) && (line - toLine == 1)))) && (column == toColumn)){
+        if (line != toLine && column != toColumn &&
+                Math.max(line, toLine) - Math.min(line, toLine) == Math.max(column, toColumn) - Math.min(column, toColumn) &&
+                checkPosition(line) && checkPosition(column) && checkPosition(toLine) && checkPosition(toColumn) &&
+                (chessBoard.board[toLine][toColumn] == null || !chessBoard.board[toLine][toColumn].color.equals(this.color))) {
+
+            if ((column == Math.min(column, toColumn) && line == Math.max(line, toLine)) ||
+                    (toColumn == Math.min(column, toColumn) && toLine == Math.max(line, toLine))) {
+                int fromL = Math.max(line, toLine);
+                int fromC = Math.min(column, toColumn);
+                int toL = Math.min(line, toLine);
+                int toC = Math.max(column, toColumn);
+                int[][] positions = new int[toC - fromC][1];
+                for (int i = 1; i < toC - fromC; i++) {
+                    if (chessBoard.board[fromL - i][fromC + i] == null) {
+                        positions[i - 1] = new int[]{fromL - i, fromC + i};
+                    } else if (!chessBoard.board[fromL - i][fromC + i].color.equals(this.color) && fromL - i == toLine) {
+                        positions[i - 1] = new int[]{fromL - i, fromC + i};
+                    } else {
+                        return false;
+                    }
+                }
                 return true;
-            } else return false;
-        }
+            } else {
+                int fromL = Math.min(line, toLine), fromC = Math.min(column, toColumn),
+                        toL = Math.max(line, toLine), toC = Math.max(column, toColumn);
+                int[][] positions = new int[toC - fromC][1];
+                for (int i = 1; i < toC - fromC; i++) {
+                    if (chessBoard.board[fromL + i][fromC + i] == null) {
+                        positions[i - 1] = new int[]{fromL + i, fromC + i};
+                    } else if (!chessBoard.board[fromL + i][fromC + i].color.equals(this.color) && fromL + i == toLine) {
+                        positions[i - 1] = new int[]{fromL + i, fromC + i};
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } else return false;
     }
 
     @Override
     public String getSymbol() {
         return "B";
+    }
+
+    public boolean checkPosition(int position){
+        if (position < 0 || position > 7){
+            return false;
+        } else return true;
     }
 }
